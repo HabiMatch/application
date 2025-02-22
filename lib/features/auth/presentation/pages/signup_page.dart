d0f2d5efd5c45b63156d2afda53d2950094da5d5
Abhishek <abhishekgupta0118@gmail.com>
2025-02-22 15:29:54 +0530

    init dependencies bloc

--- lib/features/auth/presentation/pages/signup_page.dart
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -2,9 +2,6 @@ import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:habimatch/core/theme/app_pallete.dart';
-import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
-import 'package:habimatch/core/widget/custom_snackbar.dart';
-import 'package:habimatch/features/auth/data/datasources/auth_firebase_data.dart';
 import 'package:habimatch/features/auth/presentation/bloc/auth_bloc.dart';
 
 class SignupPage extends StatefulWidget {
@@ -15,97 +12,123 @@ class SignupPage extends StatefulWidget {
 }
 
 class _SignupPageState extends State<SignupPage> {
-  firebase_auth.FirebaseAuth firebaseAuth = firebase_auth.FirebaseAuth.instance;
-  AuthFirebaseDataSourceImpl auth = AuthFirebaseDataSourceImpl();
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       appBar: AppBar(),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Column(
-              children: [
-                CircleAvatar(
-                  radius: 90,
-                  backgroundImage: AssetImage('assets/images/logoup3x.png'),
-                ),
-                SizedBox(height: 15),
-                Text(
-                  'Sign Up',
-                  style: TextStyle(
-                    fontSize: 42,
-                    fontWeight: FontWeight.bold,
-                    color: AppPallete.textColor,
+      body: BlocListener<AuthBloc, AuthState>(
+        listener: (context, state) {
+          if (state is AuthSuccess) {
+            final snackBar = SnackBar(
+              behavior: SnackBarBehavior.floating,
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              content: AwesomeSnackbarContent(
+                title: 'Success!',
+                message: 'Signed up successfully. UID: ${state.uid}',
+                contentType: ContentType.success,
+              ),
+            );
+            ScaffoldMessenger.of(context).showSnackBar(snackBar);
+          } else if (state is AuthFailure) {
+            final snackBar = SnackBar(
+              behavior: SnackBarBehavior.floating,
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              content: AwesomeSnackbarContent(
+                title: 'Error!',
+                message: state.message,
+                contentType: ContentType.failure,
+              ),
+            );
+            ScaffoldMessenger.of(context).showSnackBar(snackBar);
+          }
+        },
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                children: [
+                  const CircleAvatar(
+                    radius: 90,
+                    backgroundImage: AssetImage('assets/images/logoup3x.png'),
                   ),
-                ),
-                SizedBox(height: 20),
-                GestureDetector(
-                  onTap: () {
-                    try {
+                  const SizedBox(height: 15),
+                  Text(
+                    'Sign Up',
+                    style: TextStyle(
+                      fontSize: 42,
+                      fontWeight: FontWeight.bold,
+                      color: AppPallete.textColor,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  GestureDetector(
+                    onTap: () {
                       context.read<AuthBloc>().add(AuthSignUp());
-                    } catch (e) {}
-                  },
-
-                  child: Container(
-                    width: screenWidth * 0.85,
-                    padding: EdgeInsets.symmetric(vertical: 14, horizontal: 20),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: AppPallete.borderColor,
-                        width: 2,
+                    },
+                    child: Container(
+                      width: screenWidth * 0.85,
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 14,
+                        horizontal: 20,
                       ),
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Image.asset(
-                          'assets/images/google.png',
-                          width: 24,
-                          height: 24,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: AppPallete.borderColor,
+                          width: 2,
                         ),
-                        SizedBox(width: 20),
-                        Text(
-                          "Continue with Google",
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w500,
-                            color: AppPallete.textColor,
-                            letterSpacing: 1,
-                            wordSpacing: 1,
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'assets/images/google.png',
+                            width: 24,
+                            height: 24,
                           ),
-                        ),
-                        SizedBox(width: 20),
-                      ],
+                          const SizedBox(width: 20),
+                          Text(
+                            "Continue with Google",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                              color: AppPallete.textColor,
+                              letterSpacing: 1,
+                              wordSpacing: 1,
+                            ),
+                          ),
+                          const SizedBox(width: 20),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(height: 20),
-                Text(
-                  'Hassle-free roommate matching, just for you!',
-                  style: TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.bold,
-                    color: AppPallete.textColor,
+                  const SizedBox(height: 20),
+                  Text(
+                    'Hassle-free roommate matching, just for you!',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                      color: AppPallete.textColor,
+                    ),
                   ),
-                ),
-                SizedBox(height: 100),
-              ],
-            ),
-            Container(
-              child: Text(
+                  const SizedBox(height: 100),
+                ],
+              ),
+              const Text(
                 'Powered by https://habimatch.co.in',
                 style: TextStyle(
                   fontSize: 13,
                   fontWeight: FontWeight.bold,
-                  color: const Color.fromARGB(192, 87, 87, 87),
+                  color: Color.fromARGB(192, 87, 87, 87),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
